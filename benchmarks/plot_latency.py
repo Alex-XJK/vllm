86d086362ee377401ad6a879584afd6e7a377e25
Alex Jiakai Xu <ax2155@columbia.edu>
2024-11-05 22:49:36 -0500

    Plot latency diagram

--- benchmarks/plot_latency.py
+++ benchmarks/plot_latency.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def plot_latency(args):
     data = pd.read_csv(args.csv)
     data['x_axis'] = data['max_seq_len'] * data['batch_size']
@@ -29,7 +30,8 @@ def plot_periodic_latency(args):
     plt.figure(figsize=(10, 6))
 
     plt.bar(unique_data['x_axis'], unique_data['1st_period_sec'], label='1st Period (sec)', width=2000, alpha=0.7)
-    plt.bar(unique_data['x_axis'], unique_data['2nd_period_sec'], bottom=unique_data['1st_period_sec'], label='2nd Period (sec)', width=2000, alpha=0.7)
+    plt.bar(unique_data['x_axis'], unique_data['2nd_period_sec'], bottom=unique_data['1st_period_sec'],
+            label='2nd Period (sec)', width=2000, alpha=0.7)
 
     plt.xlabel('Max Sequence Length * Batch Size')
     plt.ylabel('Latency (sec)')
@@ -37,6 +39,39 @@ def plot_periodic_latency(args):
     plt.legend()
 
     plt.savefig(f"{args.out}_periods.png")
+
+
+def plot_prefill_latency(args, N=None):
+    data = pd.read_csv(args.csv)
+
+    if N is not None:
+        data = data[data['max_seq_len'] * data['batch_size'] == N]
+        if data.empty:
+            print(f"No categories found where max_seq_len * batch_size = {N}")
+            return
+
+    unique_categories = data[['max_seq_len', 'batch_size']].drop_duplicates()
+
+    plt.figure(figsize=(10, 6))
+    for _, row in unique_categories.iterrows():
+        current_max_seq_len = row['max_seq_len']
+        current_batch_size = row['batch_size']
+        category_data = data[(data['max_seq_len'] == current_max_seq_len) &
+                             (data['batch_size'] == current_batch_size)]
+
+        category_data = category_data.sort_values(by='chunk_size')
+
+        plt.plot(category_data['chunk_size'], category_data['p1_time_sec'],
+                 label=f'BS: {current_batch_size}, MSL: {current_max_seq_len}', )
+
+    plt.xlabel('Chunk Size')
+    plt.ylabel('Latency (sec)')
+    plt.title(f'Latency vs. Chunk Size with MSL*BS={N}')
+    plt.legend()
+    plt.grid(True)
+
+    # Save the plot
+    plt.savefig(f"{args.out}_prefill_latency.png")
 
 
 if __name__ == '__main__':
@@ -49,5 +84,6 @@ if __name__ == '__main__':
                         default='latency_results',
                         help='Output file label.')
     args = parser.parse_args()
-    plot_latency(args)
-    plot_periodic_latency(args)
+    # plot_latency(args)
+    # plot_periodic_latency(args)
+    plot_prefill_latency(args, N=2048)
